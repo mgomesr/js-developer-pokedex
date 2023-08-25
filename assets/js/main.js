2b25472aaf69ac1834b882e6b3ec6bc8b94511b6
mgomesr <mgomesr.pt@gmail.com>
2023-08-25 09:43:49 -0300

    Adicionando background nos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,13 @@ const loadFirstGeneration = document.getElementById('loadFirstGeneration');
 const loadSecondGeneration = document.getElementById('loadSecondGeneration');
 const loadThirdGeneration = document.getElementById('loadThirdGeneration');
 const loadFourthGeneration = document.getElementById('loadFourthGeneration');
+const backToTop = document.getElementById('backToTop');
 
 function chooseGeneration(){
-    loadFirstGeneration.classList.add('standard');
-    loadSecondGeneration.classList.add('standard');
-    loadThirdGeneration.classList.add('standard');
-    loadFourthGeneration.classList.add('standard');
+    loadFirstGeneration.classList.add('standardButton');
+    loadSecondGeneration.classList.add('standardButton');
+    loadThirdGeneration.classList.add('standardButton');
+    loadFourthGeneration.classList.add('standardButton');
 }
 
 function loadPokemonItems(offset, limit) {
@@ -37,7 +38,7 @@ loadFirstGeneration.addEventListener('click', () => {
     pokemonList.innerHTML = '';
     loadPokemonItems(0, 151);
     chooseGeneration();
-    loadFirstGeneration.classList.remove('standard');
+    loadFirstGeneration.classList.remove('standardButton');
     loadFirstGeneration.classList.add('focusButton');    
 });
 
@@ -45,7 +46,7 @@ loadSecondGeneration.addEventListener('click', () => {
     pokemonList.innerHTML = '';
     loadPokemonItems(151, 100);
     chooseGeneration();
-    loadSecondGeneration.classList.remove('standard');
+    loadSecondGeneration.classList.remove('standardButton');
     loadSecondGeneration.classList.add('focusButton');
 });
 
@@ -53,7 +54,7 @@ loadThirdGeneration.addEventListener('click', () => {
     pokemonList.innerHTML = '';
     loadPokemonItems(251, 135);
     chooseGeneration();
-    loadThirdGeneration.classList.remove('standard');
+    loadThirdGeneration.classList.remove('standardButton');
     loadThirdGeneration.classList.add('focusButton');
 });
 
@@ -61,6 +62,10 @@ loadFourthGeneration.addEventListener('click', () => {
     pokemonList.innerHTML = '';
     loadPokemonItems(386, 107);
     chooseGeneration();
-    loadFourthGeneration.classList.remove('standard');
+    loadFourthGeneration.classList.remove('standardButton');
     loadFourthGeneration.classList.add('focusButton');
 });
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
